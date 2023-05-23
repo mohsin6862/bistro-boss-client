e46cb63973b0557b6c2646dfbd696300e27b34b8
mohsin6862 <mohsinctg2206@gmail.com>
2023-05-23 11:57:41 +0600

    poster added

---
+++ src/Pages/Home/Poster/Poster.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Poster.css'
+
+const Poster = () => {
+    return (
+        <div className='poster text-center py-36 bg-cover my-20'>
+        <div className='w-10/12 mx-auto  bg-white p-12 '>
+        <h2 className='text-3xl mb-4 tracking-wider'>Bistro Boss</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, libero autem blanditiis voluptatum possimus rerum. Corrupti, voluptates, recusandae nisi, saepe quidem facilis in enim molestiae repellendus cumque tempore quo id aut velit consectetur reprehenderit. Asperiores sint sequi ratione ducimus doloremque molestias provident est optio minima quos aperiam, magnam culpa. Corrupti!</p>
+        </div>
+        </div>
+    );
+};
+
+export default Poster;
