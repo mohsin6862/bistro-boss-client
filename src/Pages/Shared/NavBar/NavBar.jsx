58c8fc8242c87f486babe05d136004899c64860f
mohsin6862 <mohsinctg2206@gmail.com>
2023-05-28 18:30:42 +0600

    password validation added and logOut functionality added

--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../Provider/AuthProvider/AuthProvider';
 
 const NavBar = () => {
+    const { user, LogOut } = useContext(AuthContext)
     const navItems = <>
-                  
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/menu">Our Menu</Link></li>
-                        <li><Link to="/order/salad">Order Food</Link></li>
-                        <li><Link to="/login">Log In</Link></li>
+
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/menu">Our Menu</Link></li>
+        <li><Link to="/order/salad">Order Food</Link></li>
+
+        {
+            user ? <>   <li><Link to="/login">Log out</Link></li></> : <><li><Link to="/login">Log In</Link></li>
+            </>
+        }
     </>
     return (
         <div>
@@ -18,14 +25,14 @@ const NavBar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                           {navItems}
+                            {navItems}
                         </ul>
                     </div>
                     <a className="btn btn-ghost normal-case text-xl">Bistro Boss </a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                      {navItems}
+                        {navItems}
                     </ul>
                 </div>
                 <div className="navbar-end">
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,68 +1,113 @@
-import React from 'react';
+import { useForm } from "react-hook-form";
 import { Helmet } from 'react-helmet-async';
 import signupLogo from '../../assets/others/authentication2.png'
 import './SignUp.css'
 import { Link } from 'react-router-dom';
+import { useContext } from "react";
+import { AuthContext } from "../../Provider/AuthProvider/AuthProvider";
 
 const SignUp = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const {createUser} = useContext(AuthContext)
+     const onSubmit = data => {
+        console.log(data)
+        createUser(data.email,data.password)
+        .then(result=>{
+            const newUser = result.user
+            console.log(newUser)
+        })
+        .catch(error=>{
+            console.log(error.message)
+        })
+    
+    };
+
     return (
         <div>
-             <Helmet>
+            <Helmet>
                 <title>Bistro Boss | SignUp</title>
 
             </Helmet>
-            
+
             <div className="bimg flex lg:flex-row-reverse">
-            <div className="w-1/2">
-                {/* Login Form */}
-                <div className="flex items-center justify-center h-screen">
-                    <form  className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-3/4">
-                        <h2 className="text-2xl mb-6">Sign Up</h2>
-                        <div className="mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                                Email
-                            </label>
-                            <input
-                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-100 leading-tight focus:outline-none focus:shadow-outline"
-                                id="username"
-                                type="email"
-                                placeholder="Enter Your Email"
-                                name='email'
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                                Password
-                            </label>
-                            <input
-                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                id="password"
-                                type="password"
-                                name='password'
-                                placeholder="Enter Your Password"
-                            />
-                        </div>
-             
-                     
-                        <div className="flex items-center justify-between">
-                          
-                            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline " > Sign Up</button>
-                         
-                        </div>
+                <div className="w-1/2">
+                    {/* Login Form */}
+                    <div className="flex items-center justify-center h-screen">
+                        <form  onSubmit={handleSubmit(onSubmit)} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-3/4">
+                            <h2 className="text-2xl mb-6">Sign Up</h2>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                                    Name
+                                </label>
+                                <input
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-100 leading-tight focus:outline-none focus:shadow-outline"
+                               
+                                    type="text"
+                                    placeholder="Enter Your Name"
+                                    name='name'
+                                     {...register("name" ,{ required: true })}
+                                />
+                                 {errors.name && <span className="text-red-600">Name is required</span>}
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                                    Email
+                                </label>
+                                <input
+                                    className="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none text-gray-100 focus:shadow-outline"
+                                    {...register("email" ,{ required: true })}
+                                    type="email"
+                                    placeholder="Enter Your Email"
+                                    name='email'
+                                   
+                                />
+                                  {errors.email && <span className="text-red-600">Email is required</span>}
+                            </div>
+                            <div className="mb-3">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                                    Password
+                                </label>
+                                <input
+                                    className="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none text-gray-100 focus:shadow-outline"
+                                    id="password"
+                                    type="password"
+                                    name='password'
+                                    placeholder="Enter Your Password"
+                                     {...register("password",
+                                     {
+                                         required: true,
+                                         minLength: 8,
+                                         pattern:/(?=.*[a-z])(?=.*[A-Z])(?=.*?[0-9])/
+                                        
+                                        
+                                        })}
+                                />
+                                  {errors.name && <span className="text-red-600">Password is required</span>}
+                                  {errors.password?.type === 'minLength' && <p className="text-red-600">Password must have to 6 Character</p>}
+                                  
+                                  {errors.password?.type === 'pattern' && <p className="text-red-600">Password must have atleast 1 uppercase 1 lowarcase and a digit</p>}
+                            </div>
+
+
+                            <div className="flex items-center justify-between">
+
+                                <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline " > Sign Up</button>
+
+                            </div>
                             <p className='text-center mt-5'><small>Already have an account? <Link to='/login'><span className='text-blue-500 hover:text-blue-700'>Please Login</span></Link></small></p>
-                    </form>
+                        </form>
+                    </div>
+                </div>
+                <div className="w-1/2">
+                    {/* Image */}
+
+                    <div
+                        className=" my-16"
+
+                    />
+                    <img src={signupLogo} alt="" />
                 </div>
             </div>
-            <div className="w-1/2">
-                {/* Image */}
-              
-                <div
-                    className=" my-16"
-                    
-                />
-                <img src={signupLogo} alt="" />
-            </div>
-        </div>
         </div>
     );
 };
